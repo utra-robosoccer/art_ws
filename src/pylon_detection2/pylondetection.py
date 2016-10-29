a5071328d5525cf0ff990a3555df01a6ab97b572
Jerry Jia <j1054775420@gmail.com>
2016-10-29 15:27:11 -0400

    works now. draw the contour, imutil is fordetecting the HSV value, not needed can be commented out

--- src/pylon_detection2/pylondetection.py
+++ src/pylon_detection2/pylondetection.py
@@ -3,7 +3,7 @@ from collections import deque
 import numpy as np
 import argparse
 import cv2
-import imutils
+#import imutils
  
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -11,15 +11,15 @@ ap.add_argument("-v", "--video",help="path to the (optional) video file")
 ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
 args = vars(ap.parse_args())
 
-orangeLower = (0,128,255) 
-orangeUpper = (0,200,255)
+orangeLower = (0,115,194) 
+orangeUpper = (18,255,255)
 pts = deque(maxlen = args["buffer"]) 
 
 if not args.get("video", False): 
    camera = cv2.VideoCapture(0) 
 
-else: 
-   camera = cv2.VdeioCapture(args["video"])
+#else: 
+ #  camera = cv2.VdeioCapture(args["video"])
 
 while True:
 	# grab the current frame
@@ -32,7 +32,7 @@ while True:
  
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=600)
+	#frame = imutils.resize(frame, width=800)
 	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
  
@@ -42,18 +42,25 @@ while True:
 	mask = cv2.inRange(hsv, orangeLower, orangeUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
-		# find contours in the mask and initialize the current
+	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
 	center = None
+	if len(cnts) > 0:
+		c = max(cnts, key = cv2.contourArea)
+		cv2.drawContours(frame, c, -1,(0,0,255),8)
  
+
 	# only proceed if at least one contour was found
+	'''
 	if len(cnts) > 0:
 		# find the largest contour in the mask, then use
 		# it to compute the minimum enclosing circle and
 		# centroid
 		c = max(cnts, key=cv2.contourArea)
+		cv2.drawContours(img, c, -1,0,255,-1) 
+		
 		((x, y), radius) = cv2.minEnclosingCircle(c)
 		M = cv2.moments(c)
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -65,6 +72,8 @@ while True:
 			cv2.circle(frame, (int(x), int(y)), int(radius),
 				(0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		'''
+	
  
 	for i in xrange(1, len(pts)):
 		# if either of the tracked points are None, ignore
@@ -76,6 +85,7 @@ while True:
 		# draw the connecting lines
 		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
 	# update the points queue
 	pts.appendleft(center)
 	cv2.imshow("Frame", frame)
